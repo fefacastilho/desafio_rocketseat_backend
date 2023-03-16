2bd189e06d9fd947e21231c072583b5cc5c0507d
fernanda <castilhogomesoliveira@gmail.com>
2023-03-16 14:08:20 -0300

    adding backend

---
+++ src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,14 @@
+const creatUsers = `
+   CREATE TABLE IF NOT EXISTS  users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR,
+    email VARCHAR,
+    password VARCHAR,
+    avatar VARCHAR NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UPDATED_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+  
+  `;
+
+  module.exports = creatUsers;
